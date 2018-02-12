5165e0c74da94a2069df8c922503d3ab160969c9
Jean-Félix BENITEZ <jeanfelix.benitez@gmail.com>
2018-02-12 21:16:35 +0100

    Début de rédaction de l'analyse des exigences.

--- src/doc/avancement/gen.cmd
+++ src/doc/avancement/gen.cmd
@@ -1,4 +1,10 @@
 @REM -*- coding:cp850-dos; -*-
 @echo off
+if "%1" == "" goto manqueFTex
 texify -c -b -p -q %1
 move /Y %~n1.pdf ..\..\..\target\doc\avancement\
+goto fin
+:manqueFTex
+echo Vous devez passer en paramätre le fichier TEX Ö compiler !
+pause
+:fin
